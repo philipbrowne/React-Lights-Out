66f621f7b90e44c6b5361ba659426f55f6a7eee9
Philip Browne <pbrowne@gmail.com>
2021-09-28 15:11:50 -0400

    update description

--- src/components/GameRules/GameRules.js
+++ src/components/GameRules/GameRules.js
@@ -6,10 +6,11 @@ const GameRules = () => {
     <div className="GameRules">
       <p>
         The game consists of a grid of lights which is 5x5 by default. When the
-        game starts, a random number or a stored pattern of these lights is
-        switched on. Pressing any of the lights will toggle it and the four
-        adjacent lights. The goal of the puzzle is to switch all the lights off,
-        preferably in as few button presses as possible.
+        game starts, a random number of these lights is switched on. The game
+        default is a 50% chance of being lit. Pressing any of the lights will
+        toggle it and the four adjacent lights. The goal of the puzzle is to
+        switch all the lights off, preferably in as few button presses as
+        possible.
       </p>
     </div>
   );
